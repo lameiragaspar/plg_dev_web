05681c22272260abe4c6593857eef1f944fc6c15
Pedro Lameira <120721002+lameiragaspar@users.noreply.github.com>
2024-01-22 21:54:04 +0100

    LoadingPage

--- src/Paginas/LoadingPage/loadingPage.js
+++ src/Paginas/LoadingPage/loadingPage.js
@@ -1,13 +1,23 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Formulario from '../../Componentes/footer/formulario/form'
+import './loading.css'
 
 export default function LoadingPage(){
     return(
-        <>
-            <h2>
-                Está é a LandingPage!
-            </h2>
-            <Link to='/' className='a'>Home</Link>
-        </>
+        <section className='loadingPage'>
+            {/**/}
+            <div className='logoEform'>
+                <div className='imagemLogo'>
+                    <img src='/assets/logo.jpg'/>
+                </div>
+                <div className='boasVindas'>
+                    <nav>
+                        <Link to='/' className='link'>Home</Link>
+                    </nav>
+                    <Formulario />
+                </div>
+            </div>
+        </section>
     )
 }
