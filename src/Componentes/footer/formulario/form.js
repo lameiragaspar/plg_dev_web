452e4b15e92994d8febffa55253e3171d116cc31
Pedro Lameira <120721002+lameiragaspar@users.noreply.github.com>
2024-01-26 20:33:07 +0100

    Form 2

--- src/Componentes/footer/formulario/form.js
+++ src/Componentes/footer/formulario/form.js
@@ -1,35 +1,35 @@
 import React,{useState} from "react";
 import './formulario.css'
 
-const Formulario = () =>{
-    return(
+function Formulario() {
+    return (
         <div className="formulario">
-        <form>
-            <p>Ou envie uma mensagem aqui</p>
-            <div className="dadosPessoais">
-                <div>
-                    <input type="text" name="nome" id="nome" placeholder="nome completo" color="black" required/>
-                    <label for="nome">Nome</label>
+            <form action="mailto:peldrolameira20@gmail.com" method="post" encType="text/plain">
+                <p>Ou envie uma mensagem aqui</p>
+                <div className="dadosPessoais">
+                    <div>
+                        <label htmlFor="nome">Nome</label>
+                        <input type="text" id="nome" name="nome" placeholder="Seu nome completo" required />
+                    </div>
+                    <div>
+                        <label htmlFor="email">Email</label>
+                        <input type="email" id="email" name="email" placeholder="Seu email" required />
+                    </div>
                 </div>
-                <div>
-                    <input type="email" name="email" id="email" placeholder="email" required/>
-                    <label for="email">Email</label>
+                <div className="mensagem">
+                    <div>
+                        <label htmlFor="assunto">Assunto</label>
+                        <input type="text" id="assunto" name="assunto" placeholder="Assunto da mensagem" required />
+                    </div>
+                    <div className="txeEbotao">
+                        <label htmlFor="sms">Sua mensagem</label>
+                        <textarea id="sms" name="mensagem" placeholder="Escreva sua mensagem aqui" required></textarea>
+                        <button type="submit">Enviar</button>
+                    </div>
                 </div>
-            </div>
-            <div className="mensagem">
-                <div> 
-                    <input type="text" name="assunto" id="assunto" placeholder="assunto" required/>
-                    <label for="assunto">Assunto</label>
-                </div>
-                <div className="txeEbotao">
-                    <input type="text" name="sms" id="sms" placeholder="escreva aqui" required />
-                    <label for="sms">sua mensagem</label>
-                    <button>Enviar</button>
-                </div>
-            </div>
-        </form>
-    </div>
-    )
+            </form>
+        </div>
+    );
 }
-    
+
 export default Formulario
