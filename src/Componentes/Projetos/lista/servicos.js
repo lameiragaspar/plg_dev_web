1e291e476ee205d073ee01fecbc76675091b5831
Pedro Lameira <120721002+lameiragaspar@users.noreply.github.com>
2024-02-13 08:42:10 +0100

    Atualização

--- src/Componentes/Projetos/lista/servicos.js
+++ src/Componentes/Projetos/lista/servicos.js
@@ -4,9 +4,9 @@ export const retornaProjeto = () => {
     return porjetos.slice(0, 6)
 }
 
-export const BuscaProjeto = (txtPesquisado) =>{
-    return(porjetos.filter(projeto =>
+export const BuscaProjeto = (txtPesquisado) => (
+    porjetos.filter(projeto =>
     projeto.titulo.toLocaleLowerCase().includes(txtPesquisado.toLocaleLowerCase()) ||
     projeto.tecnologia.toLocaleLowerCase().includes(txtPesquisado.toLocaleLowerCase())
-    ))
-}
+    )
+)
